8f765d68f7aa48a1fa6a186392ce748140ec9882
twardoch <adam@twardoch.com>
2023-03-31 08:52:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@ dir=${0%/*}
 if [ "$dir" = "$0" ]; then dir="."; fi
 cd "$dir"
 echo "Running: brew install snappy protobuf boost"
-brew uninstall --ignore-dependencies snappy protobuf boost && brew install snappy protobuf boost 
+brew uninstall --ignore-dependencies snappy protobuf boost && brew install snappy protobuf boost && brew link --overwrite protobuf
 # echo "Extracting protos from /Applications/Keynote.app..."
 # cd keynote-protos
 # ./get-protos.sh
 ## THIS FAILS ## ./dump-mappings.sh
 # cd ..
 
+git submodule update --init --recursive
 arch=$(uname -m)
 
 echo "Generating Makefile for '$arch'..."
