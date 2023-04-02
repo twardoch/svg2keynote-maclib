4b6b2579abfbcb60009563ec0549d834d7eb8210
twardoch <adam@twardoch.com>
2023-04-02 04:27:21 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ if [ "$dir" = "$0" ]; then dir="."; fi
 cd "$dir"
 git submodule update --init --recursive
 echo "Running: brew install snappy protobuf boost"
-brew uninstall --ignore-dependencies snappy protobuf boost && brew install snappy protobuf boost && brew link --overwrite protobuf
+brew uninstall --ignore-dependencies snappy protobuf boost
+brew install snappy protobuf boost && brew link --overwrite protobuf
 echo "Extracting protos from /Applications/Keynote.app..."
 cd keynote-protos
 ./get-protos.sh
